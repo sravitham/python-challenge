5e12f7c9f668ef60b4e4391131ff8e1c7a2697b1
sravitham <sravitha.matlapudi@gmail.com>
2022-04-05 00:02:08 -0400

    added comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,23 +11,28 @@ with open(csv_path_input) as csvfile:
     #CSV reader specifies delimiter and variable that holds contents
     csv_reader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csv_reader)
-
+    
+    #creating indexed list to contain unique candidates
     candidates_list = []
+    #create a dictionary that contains the total votes for each candidate
     candidate_votes = {}
-
+    #intialize variables
     total_votes = 0
 
+#readind through each row in the csv file
     for row in csv_reader:
+        #add a vote for each new row
         total_votes += 1
-
+        #get first candidate name in the dataset
         candidate_name = (row[2])
-
+        #check whether the candiate is in the list before appending and initialize the vote count
         if candidate_name not in candidates_list:
             candidates_list.append(candidate_name)
             candidate_votes[candidate_name] = 0
-
+        #add a vote count for each candidate
         candidate_votes[candidate_name] += 1
 
+#print analysis to terminal
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
@@ -45,6 +50,7 @@ print("-------------------------")
 winner = max(candidate_votes, key=candidate_votes.get)
 print("Winner:",winner)
 
+#print output file to text file
 with open('PyPoll\Analysis\Analysis.txt', 'w') as text:
     text.write("Election Results\n")
     text.write("-------------------------\n")
